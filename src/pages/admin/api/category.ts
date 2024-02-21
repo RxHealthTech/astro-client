1e0237fdf9330aa4d500afa8aac246c71b944785
Sergio Wilson <sergio.wilson@foxpoint.com>
2024-02-21 16:54:12 -0600

    introduce list view

---
+++ src/pages/admin/api/category.ts
@@ -0,0 +1,15 @@
+import type {APIRoute} from "astro";
+
+export const GET: APIRoute = ({ params, request }) => {
+  return new Response(JSON.stringify({
+      message: "This was a GET!"
+    })
+  )
+}
+
+export const POST: APIRoute = ({ request }) => {
+  return new Response(JSON.stringify({
+      message: "This was a POST!"
+    })
+  )
+}
