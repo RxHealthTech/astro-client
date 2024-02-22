fbd00fd04364fa553ed6d028b5c9c4e647c77591
Sergio Wilson <sergio.wilson@foxpoint.com>
2024-02-21 18:22:48 -0600

    Finalize Remove View

--- src/pages/admin/api/category.ts
+++ src/pages/admin/api/category.ts
@@ -6,10 +6,20 @@ export const GET: APIRoute = ({ params, request }) => {
     })
   )
 }
+//
+// export const POST: APIRoute = ({ request }) => {
+//   return new Response(JSON.stringify({
+//       message: "This was a POST!"
+//     })
+//   )
+// }
 
-export const POST: APIRoute = ({ request }) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const DELETE: APIRoute = ({ params, request }) => {
+  const id = params.id;
+  
   return new Response(JSON.stringify({
-      message: "This was a POST!"
+      message: "This was a DELETE!"
     })
   )
 }
