82feae49190ecc25be0e94b0eb2a6c6aa7ff1bb4
Sergio Wilson <sergio.wilson@foxpoint.com>
2024-03-08 14:54:35 +0100

    Introduce user in payload

--- src/pages/admin/api/file/add.ts
+++ src/pages/admin/api/file/add.ts
@@ -12,17 +12,6 @@ export const POST: APIRoute = async ({ request }) => {
     body: data,
   });
   
-  // const email = data.get("email");
-  // const message = data.get("message");
-  // // Validate the data - you'll probably want to do more than this
-  // if (!name || !email || !message) {
-  //   return new Response(
-  //     JSON.stringify({
-  //       message: "Missing required fields",
-  //     }),
-  //     { status: 400 }
-  //   );
-  // }
   // Do something with the data, then return a success response
   return new Response(
     JSON.stringify({
