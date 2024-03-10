49aa7336b5005b60d9c46448c507ee0d6ec15c7f
Sergio Wilson <sergio.wilson@foxpoint.com>
2024-03-10 13:50:14 +0100

    introduce api payload

--- src/pages/admin/api/file/add.ts
+++ src/pages/admin/api/file/add.ts
@@ -1,22 +1,23 @@
-import type { APIRoute } from "astro";
+import type {APIRoute} from "astro";
+import {ApiActionResponseType} from "~/models";
 
-export const POST: APIRoute = async ({ request }) => {
+export const POST: APIRoute = async ({request}) => {
+
+  const apiResponse: ApiActionResponseType = {success: true};
 
   const api = import.meta.env.API_PATH;
   const data = await request.formData();
-  const name = data.get("name");
  
   const method = `${api}/upload`;
   const response = await fetch(method, {
     method: "POST",
     body: data,
   });
-  
-  // Do something with the data, then return a success response
-  return new Response(
-    JSON.stringify({
-      message: "Success!"
-    }),
-    { status: 200 }
-  );
+
+  if (response.ok) {
+    return new Response(JSON.stringify(apiResponse), {status: 200});
+  }
+
+  apiResponse.success = false;
+  return new Response(JSON.stringify(apiResponse), {status: 200});
 };
