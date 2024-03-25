4c2f78f899f7fe00e90fc866a7b84ed0196c6c6a
Sergio Wilson <sergio.wilson@foxpoint.com>
2024-03-25 06:34:43 -0500

    Finalize nutrition.astro Finalize Report Viewer

--- src/pages/admin/api/report/nutrition/add.ts
+++ src/pages/admin/api/report/nutrition/add.ts
@@ -1,18 +1,22 @@
 import type {APIRoute} from "astro";
-import type {ApiActionResponseType} from "~/models";
+import type { ApiActionResponseType} from '~/models'
 
 export const POST: APIRoute = async ({request}) => {
 
   const apiResponse: ApiActionResponseType = {success: true};
 
   const api = import.meta.env.API_PATH;
-  const data = await request.formData();
- 
-  const method = `${api}/upload`;
+  const data = await request.json();
+  const body = JSON.stringify(data)
+  
+  const method = `${api}/report/nutrition`;
   const response = await fetch(method, {
-    method: "POST",
-    body: data,
-  });
+   method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body
+  })
 
   if (response.ok) {
     return new Response(JSON.stringify(apiResponse), {status: 200});
@@ -20,4 +24,4 @@ export const POST: APIRoute = async ({request}) => {
 
   apiResponse.success = false;
   return new Response(JSON.stringify(apiResponse), {status: 200});
-};
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -18,6 +18,9 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio')
+  ],
   darkMode: 'class',
 };
